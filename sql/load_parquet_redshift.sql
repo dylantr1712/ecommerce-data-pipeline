81027163f8c04a5e9c28f4684fe599d96de38d27
dylantr1712 <dylantruong1712@gmail.com>
2026-03-28 12:57:14 +1100

    Add Redshift schemas and staging load SQL
    Create Redshift schemas and staging tables for raw ecommerce datasets (orders, customers, order_items, order_payments, order_reviews, products, sellers, product_category_translation). Add a loader script with COPY commands to ingest Parquet files from S3 to Redshift.

---
+++ sql/load_parquet_redshift.sql
@@ -0,0 +1,48 @@
+copy staging.stg_orders_raw
+from 's3://dylan-ecommerce-data-pipeline/silver/orders/'
+iam_role 'arn:aws:iam::313828097071:role/RedshiftS3ReadRole'
+format as parquet;
+
+copy staging.stg_customers_raw
+from 's3://dylan-ecommerce-data-pipeline/silver/customers/'
+iam_role 'arn:aws:iam::313828097071:role/RedshiftS3ReadRole'
+format as parquet;
+
+copy staging.stg_order_items_raw
+from 's3://dylan-ecommerce-data-pipeline/silver/order_items/'
+iam_role 'arn:aws:iam::313828097071:role/RedshiftS3ReadRole'
+format as parquet;
+
+copy staging.stg_order_payments_raw
+from 's3://dylan-ecommerce-data-pipeline/silver/order_payments/'
+iam_role 'arn:aws:iam::313828097071:role/RedshiftS3ReadRole'
+format as parquet;
+
+copy staging.stg_order_reviews_raw
+from 's3://dylan-ecommerce-data-pipeline/silver/order_reviews/'
+iam_role 'arn:aws:iam::313828097071:role/RedshiftS3ReadRole'
+format as parquet;
+
+copy staging.stg_products_raw
+from 's3://dylan-ecommerce-data-pipeline/silver/products/'
+iam_role 'arn:aws:iam::313828097071:role/RedshiftS3ReadRole'
+format as parquet;
+
+copy staging.stg_sellers_raw
+from 's3://dylan-ecommerce-data-pipeline/silver/sellers/'
+iam_role 'arn:aws:iam::313828097071:role/RedshiftS3ReadRole'
+format as parquet;
+
+copy staging.stg_product_category_translation_raw
+from 's3://dylan-ecommerce-data-pipeline/silver/product_category_name_translation/'
+iam_role 'arn:aws:iam::313828097071:role/RedshiftS3ReadRole'
+format as parquet;
+
+select count(*) from staging.stg_customers_raw;
+select count(*) from staging.stg_orders_raw;
+select count(*) from staging.stg_order_items_raw;
+select count(*) from staging.stg_order_payments_raw;
+select count(*) from staging.stg_order_reviews_raw;
+select count(*) from staging.stg_products_raw;
+select count(*) from staging.stg_sellers_raw;
+select count(*) from staging.stg_product_category_translation_raw;
